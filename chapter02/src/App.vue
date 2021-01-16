048d190b4689191e0413bbd28cc0ece61301fbdd
DanielGara <yo@danielgara.com>
2021-01-16 15:35:07 -0500

    imp: chapter 02 star rating

--- chapter02/src/App.vue
+++ chapter02/src/App.vue
@@ -10,9 +10,29 @@
         The path of the righteous man is beset on all sides by the iniquities
         of the selfish and the tyranny of evil men.`"
     >
+    <p>
+        <StarRating
+          ref="starRating"
+        />
+      </p>
+      <p>
+        The path of the righteous man is beset on all sides by the iniquities
+        of the selfish and the tyranny of evil men.
+      </p>
       <template v-slot:action>
-        <button>Action Button</button>
-        <button>Action Button</button>
+        <MaterialButton
+          background-color="#027be3"
+          text-color="#fff"
+        >
+          Action 1
+        </MaterialButton>
+        <MaterialButton
+          background-color="#26a69a"
+          text-color="#fff"
+          is-flat
+        >
+          Action 2
+        </MaterialButton>
       </template>
     </MaterialCardBox>
   </div>
@@ -20,10 +40,14 @@
 
 <script>
 import MaterialCardBox from './components/MaterialCardBox.vue';
+import MaterialButton from './components/MaterialButton.vue';
+import StarRating from './components/StarRating.vue';
 export default {
   name: 'App',
   components: {
+    MaterialButton,
     MaterialCardBox,
+    StarRating,
   },
 };
 </script>
